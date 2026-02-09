c494dba121d6c1a0a5a2f934cac05886ca310d94
joshhsu123 <josh.hsu123@gmail.com>
2026-02-09 13:31:35 +0800

    Modify for test

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root(name: str = "PyCharm78787ForJenkins"):
+def read_root(name: str = "PyCharm78787ForJenkinsTest"):
     """Root endpoint. Optional query param `name` (default: PyCharm)."""
     return {"message": f"Hi, {name}"}
 
